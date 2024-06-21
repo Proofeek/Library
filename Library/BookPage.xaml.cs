be9922fa469fa97f74ee6de7910423496623f545
Proofeek <kiill@yandex.ru>
2024-06-21 11:52:05 +0400

    Дополнительная литература сделана

--- Library/BookPage.xaml.cs
+++ Library/BookPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -28,14 +29,29 @@ namespace Library
         {
             InitializeComponent();
             Book = book;
+
+            // Заполнение похожей литературы
             List<Book> similarBooks = dbHelper.FindSimilarBooks(Book);
-            BooksListBox.ItemsSource = similarBooks;
+            if (similarBooks.Any()) SimilarListBox.ItemsSource = similarBooks;
+            else TextSimilar.Visibility = Visibility.Collapsed;
+
+            // Заполнение дополнительной литературы
+            List<Book> additionalLiterature = dbHelper.FindAdditionalLiterature(Book);
+            if (additionalLiterature.Count > 0) CC1.Content = additionalLiterature[0];
+            else
+            {
+                CC1.Visibility = Visibility.Collapsed;
+                TextAdditional.Visibility = Visibility.Collapsed;
+            }
+            CC2.Content = additionalLiterature.Count > 1 ? additionalLiterature[1] : CC2.Visibility = Visibility.Collapsed;
+            CC3.Content = additionalLiterature.Count > 2 ? additionalLiterature[2] : CC3.Visibility = Visibility.Collapsed;
+
             this.DataContext = this;
         }
 
-        private void BooksListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (BooksListBox.SelectedItem is Book selectedBook)
+            if (SimilarListBox.SelectedItem is Book selectedBook)
             {
                 BookPage bookPage = new BookPage(selectedBook);
                 if (Window.GetWindow(this) is MainWindow mainWindow)
--- Library/DataBaseService.cs
+++ Library/DataBaseService.cs
@@ -216,5 +216,109 @@ namespace Library
 
             return similarBooks;
         }
+        public List<Book> FindAdditionalLiterature(Book selectedBook)
+        {
+            List<Book> additionalLiterature = new List<Book>();
+
+            // Проверяем, что жанр выбранной книги соответствует условиям
+            //if (selectedBook.Genre.GenreId == 2 ||
+            //    selectedBook.Genre.GenreId == 3 ||
+            //    selectedBook.Genre.GenreId == 4 ||
+            //    selectedBook.Genre.GenreId == 10)
+            //{
+                string query = @"
+            SELECT 
+                b.BookId, b.Title, b.Annotation, b.YearPublished, b.PageCount, b.ReadingRoomNumber, b.IsAvailable, b.ImageUrl,
+                g.GenreId, g.GenreName,
+                a.AuthorId, a.FirstName, a.MiddleName, a.LastName,
+                p.PublisherId, p.PublisherName
+            FROM 
+                Books b
+            JOIN 
+                Genres g ON b.GenreId = g.GenreId
+            JOIN 
+                Authors a ON b.AuthorId = a.AuthorId
+            JOIN 
+                Publishers p ON b.PublisherId = p.PublisherId
+            WHERE 
+                g.GenreId <> @GenreId
+                AND b.BookId <> @BookId";
+
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("GenreId", selectedBook.Genre.GenreId);
+                        command.Parameters.AddWithValue("BookId", selectedBook.BookId);
+
+                        using (NpgsqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // Кеширование жанра
+                                int genreId = reader.GetInt32(reader.GetOrdinal("GenreId"));
+                                if (!genresCache.TryGetValue(genreId, out Genre genre))
+                                {
+                                    genre = new Genre
+                                    {
+                                        GenreId = genreId,
+                                        GenreName = reader.GetString(reader.GetOrdinal("GenreName"))
+                                    };
+                                    genresCache[genreId] = genre;
+                                }
+
+                                // Кеширование автора
+                                int authorId = reader.GetInt32(reader.GetOrdinal("AuthorId"));
+                                if (!authorsCache.TryGetValue(authorId, out Author author))
+                                {
+                                    author = new Author
+                                    {
+                                        AuthorId = authorId,
+                                        FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                        MiddleName = reader.IsDBNull(reader.GetOrdinal("MiddleName")) ? null : reader.GetString(reader.GetOrdinal("MiddleName")),
+                                        LastName = reader.GetString(reader.GetOrdinal("LastName"))
+                                    };
+                                    authorsCache[authorId] = author;
+                                }
+
+                                // Кеширование издателя
+                                int publisherId = reader.GetInt32(reader.GetOrdinal("PublisherId"));
+                                if (!publishersCache.TryGetValue(publisherId, out Publisher publisher))
+                                {
+                                    publisher = new Publisher
+                                    {
+                                        PublisherId = publisherId,
+                                        PublisherName = reader.GetString(reader.GetOrdinal("PublisherName"))
+                                    };
+                                    publishersCache[publisherId] = publisher;
+                                }
+
+                                // Создание объекта Book
+                                Book book = new Book
+                                {
+                                    BookId = reader.GetInt32(reader.GetOrdinal("BookId")),
+                                    Title = reader.GetString(reader.GetOrdinal("Title")),
+                                    Genre = genre,
+                                    FullAnnotation = reader.GetString(reader.GetOrdinal("Annotation")),
+                                    YearPublished = reader.GetInt32(reader.GetOrdinal("YearPublished")),
+                                    Author = author,
+                                    Publisher = publisher,
+                                    PageCount = reader.GetInt32(reader.GetOrdinal("PageCount")),
+                                    ReadingRoomNumber = reader.GetInt32(reader.GetOrdinal("ReadingRoomNumber")),
+                                    IsAvailable = reader.GetBoolean(reader.GetOrdinal("IsAvailable")),
+                                    ImageUrl = reader.IsDBNull(reader.GetOrdinal("ImageUrl")) ? null : reader.GetString(reader.GetOrdinal("ImageUrl"))
+                                };
+
+                                additionalLiterature.Add(book);
+                            }
+                        }
+                    }
+                }
+            //}
+
+            return additionalLiterature.Take(3).ToList();
+        }
     }
 }
